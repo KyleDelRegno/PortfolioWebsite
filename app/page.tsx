707e7004bda9b9af821099328659119f5c57ff57
KyleDelRegno <kdelreg@gmail.com>
2026-01-06 00:17:38 -0600

    final touches

--- app/page.tsx
+++ app/page.tsx
@@ -40,36 +40,58 @@ const HomePage : React.FC = () => {
   return (
 
     <main>
-      <section className="h-400 bg-[url(/Images/Wallpaper3.png)] sm:bg-[url(/Images/Wallpaper2.png)] bg-no-repeat bg-local saturate-120
-      bg-right bg-cover
+      <section className="h-svh bg-[url(/Images/Wallpaper3.png)] sm:bg-[url(/Images/Wallpaper2.png)] bg-no-repeat bg-local saturate-120
+      bg-top-right bg-cover
       ">
         <div className="mx-auto flex items-center justify-center p-4 sm:p-10 pt-5">
           <div className="@LOGO">
-            <img src="logo.png" className="w-15"></img>
+            <img src="logo.svg" className="w-20"></img>
           </div>
           <div className="@Space grow"></div>
           <div className="@Menu flex items-center justify-end">
-            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer ">Featured</a></div>
-            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer ">Projects</a></div>
-            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer">About Me</a></div>
-            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><button className="cursor-pointer rounded-4xl border-2 border-white p-2 px-4 hover:bg-white hover:text-gray-800">Connect</button></div>
+            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer " href="#featured">Featured</a></div>
+            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer " href="#projects">Projects</a></div>
+            <div className="ml-5 text-sm text-white md:ml-10 md:text-xl"><a className="cursor-pointer rounded-4xl border-2 border-white p-2 px-4 hover:bg-white hover:text-gray-800" href="#contact">Connect</a></div>
           </div>
         </div>
-        <div className="flex justify-center p-5">
+        <div className="flex justify-center p-10">
           <div>
             <h1 className="font-roboto font-black text-6xl text-white sm:text-9xl pt-65">Kyle DelRegno</h1>
-            <h2 className="font-roboto font-bold text-gray-300 text-xl pt-8">Videographer | Photographer | Creative Producer</h2>
+            <h2 className="font-roboto font-bold text-gray-300 text-xl pt-8">Cinematographer | Creative Producer | Photographer</h2>
+            <h3 className="font-manrope text-gray-300 text-xl pt-8 w-200">I’m Kyle DelRegno, a film major and photographer based in Chicago. I focus on telling stories through film and photography in a way that feels natural and real. I enjoy capturing atmosphere and everyday moments, whether that’s on set, on the street, or behind the scenes.</h3>
           </div>
           <div className="@Space grow max-w-200"></div>
         </div>
       </section>
-      <section className="h-fit bg-amber-50 pt-3 gap-1">
+      <section className=" h-fit bg-amber-50 pt-3 gap-1 pb-20" id="featured">
+        <h1 className="flex justify-center text-5xl sm:text-6xl text-black font-roboto font-bold mt-5 p-3 ">Featured Projects</h1>
+        <div className="flex justify-center">
+          <div className="w-100 h-1 bg-black mb-10"></div>
+        </div>
+        
         <FeaturedItemDisplay id="1" side="left"></FeaturedItemDisplay>
         <FeaturedItemDisplay id="3" side="right"></FeaturedItemDisplay>
       </section>
-      <section className="h-fit bg-amber-50 pt-10">
+      <section className="h-fit bg-amber-50 pt-5 pb-30" id="projects">
+        <h1 className="flex justify-center text-5xl sm:text-6xl text-black font-roboto font-bold p-3">Projects</h1>
+        <div className="flex justify-center">
+          <div className="w-40 h-1 bg-black mb-10"></div>
+        </div>
         <GridComponent onSelect={triggerSelected}></GridComponent>
         {selected_id && selected_data && (<DisplayItem project={selected_id} projectData={selected_data} onClose={triggerSelected}></DisplayItem>)}
+      </section>
+      <section className="h-fit bg-amber-50 pb-7 pt-7 border-t-4 border-black" id="contact">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
+            <h1 className="text-5xl text-black pb-1 font-manrope font-bold inline sm:hidden lg:inline">Connect</h1>
+            <div className="bg-black h-15 w-1 hidden lg:inline"></div>
+            <h1 className="text-black text-3xl ">443-547-7253</h1>
+            <h1 className="text-black text-3xl ">kyledelregno@gmail.com</h1>
+            <div className="flex flex-row justify-center items-center gap-10">
+            <a href="https://www.linkedin.com/in/kyle-delregno/" target="blank" className="w-9"><img src="linked.svg" alt="Linked In  "></img></a>
+            <a href="https://www.instagram.com/kyledelregnoportfolio/" target="blank" className="w-10"><img src="insta.svg" alt="Instagram  " className="text-black"></img></a>
+            <a href="https://www.youtube.com/@kyledelregno" target="blank" className="w-12"><img src="tube.svg" alt="Youtube"></img></a>
+            </div>
+        </div>
       </section>
     </main>
   );
--- components/DisplayItem.tsx
+++ components/DisplayItem.tsx
@@ -30,6 +30,7 @@ export default function DisplayItem({project, projectData, onClose} : ItemDispla
                         <div className="mt-4">
                             <h1 className=" text-black text-5xl lg:text-6xl xl:text-8xl text-right font-bogle font-medium">{project}</h1>
                             <h1 className=" text-gray-700 text-lg xl:text-xl text-right font-manrope">{projectData.date}</h1>
+                            <p className=" text-gray-700 text-md xl:text-lg text-right mt-2 font-manrope">{projectData.description}</p>
                         </div>
                         <div className="flex flex-col sm:flex-row items-end sm:justify-between mt-2">
                             {projectData.link!=="none" ? <div className="flex w-fit justify-end m-1 sm:m-0"><a href={projectData.link} target="_blank" className="text-center cursor-pointer rounded-4xl p-1.5 px-4 border-white text-white bg-blue-500 hover:bg-blue-300 ">View Project Here</a></div> : <div></div>}
@@ -63,7 +64,8 @@ export default function DisplayItem({project, projectData, onClose} : ItemDispla
                     <div className="flex flex-col w-full p1-4 pr-4 basis-1/3  justify-between">
                         <div className="mt-4">
                             <h1 className=" text-black text-5xl lg:text-6xl xl:text-8xl text-right font-bogle font-medium">{project}</h1>
-                            <h1 className=" text-gray-700 text-lg xl:text-xl text-right font-manrope">{projectData.date}</h1>
+                            <h1 className=" text-gray-700 text-lg lg:text-xl text-right font-manrope">{projectData.date}</h1>
+                            <p className=" text-gray-700 text-md lg:text-lg text-right mt-2 font-manrope">{projectData.description}</p>
                         </div>
                         <div className="flex flex-col sm:flex-row justify-between mt-2">
                             {projectData.link!=="none" ? <div className="sm:text-left justify-items-end place-content-end m-1 mb-2 mt-2 sm:mb-0 sm:mt-0"><a href={projectData.link} target="_blank" className="flex flex-nowrap text-center w-fit cursor-pointer rounded-4xl border-2 p-1 px-4 border-white text-white bg-blue-500 hover:bg-blue-300 ">View Project Here</a></div> : <div></div>}
